8ed12423b2292a73a3c30c6bd4bd08d4cb0d6cb2
Ghanshyam Jadhav <gkkjadhav@gmail.com>
2024-03-02 12:14:25 +0530

    Add project files.

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankTransaction.Models;
+using BankTransactions.Models;
+
+namespace BankTransactions.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
+        {
+            if(id == 0)
+                return View(new Transaction());
+            else
+                return View(_context.Transactions.Find(id));
+        }
+
+        // POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {  // this is server side validation
+
+                if(transaction.TransactionId == 0)
+                {
+                    transaction.Date = DateTime.Now;
+                    _context.Add(transaction);
+                }
+                else
+                {
+                    _context.Update(transaction);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+        // its not best practice to do delete operation with get method
+        // because by simply typing url on browser :- delete/5 anyone can delete
+        // id no by 5
+       
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BankTransaction.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
